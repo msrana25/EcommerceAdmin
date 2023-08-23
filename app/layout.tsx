6a53726449270200fb2f1f6cd4941d79f1100e89
msrana25 <mnprtrana61@gmail.com>
2023-08-23 19:14:51 -0400

    Form components and Prisma, PlanetScale, MySQL setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
-import { ModalProvider } from '@/components/modals/providers/modal-provider'
+import { ModalProvider } from '@/providers/modal-provider'
 
 import './globals.css'
+import { ToasterProvider } from '@/providers/toast-provider'
+import prismadb from '@/lib/prismadb'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Admin Dashboard',
   description: 'Admin Dashboard',
 }
@@ -21,6 +23,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
+        <ToasterProvider />
         <ModalProvider />
         {children}</body>
     </html>
