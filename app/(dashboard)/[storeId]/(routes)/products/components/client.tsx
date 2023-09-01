b928a155943a9b588fbd8e30bf956dab05e20537
msrana25 <mnprtrana61@gmail.com>
2023-08-31 20:01:54 -0400

    Completed Products Entity

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -13,7 +13,6 @@ interface ProductClientProps{
   data: ProductColumn[]
 }
 
-//6:37:16
 export const ProductClient: React.FC<ProductClientProps> = ({
   data
 }) => {
@@ -25,19 +24,19 @@ export const ProductClient: React.FC<ProductClientProps> = ({
     <>
      <div className="flex items-center justify-between">
       <Heading
-      title = {`Billboards (${data.length})`}
-      description="Manage billboards for your store"
+      title = {`Products (${data.length})`}
+      description="Manage products for your store"
       />
-      <Button onClick={() => router.push(`/${params.storeId}/billboards/new`) }>
+      <Button onClick={() => router.push(`/${params.storeId}/products/new`) }>
         <Plus className="mr-2 h-4 w-4"/>
         Add New
       </Button>
      </div>
      <Separator />
-     <DataTable searchKey = "label" columns={columns} data={data}/>
-     <Heading title="API" description="API calls for Billboards"/>
+     <DataTable searchKey = "name" columns={columns} data={data}/>
+     <Heading title="API" description="API calls for Products"/>
      <Separator />
-     <ApiList entityName="billboards" entityIdName="billboardId"/>
+     <ApiList entityName="products" entityIdName="productId"/>
     </>
    ) 
 }
