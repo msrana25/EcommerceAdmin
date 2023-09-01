8e27f98ed49e06112e508d0b7f26c6076777382b
msrana25 <mnprtrana61@gmail.com>
2023-09-01 19:58:47 -0400

    Completed Order Entity and Dashboard

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -39,6 +39,8 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({
     const router = useRouter();
     const origin = useOrigin();
 
+    
+
     const[open, setOpen] = useState(false);
     const[loading, setLoading] = useState(false);
 
