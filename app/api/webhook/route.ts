1c8d98bc830471e901f1a952429b82665d0b31ef
msrana25 <mnprtrana61@gmail.com>
2023-09-07 21:49:30 -0400

    working on stripe setup and checkout functionality

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,38 @@
+import Stripe from "stripe";
+import { headers } from "next/headers"
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+import { add } from "date-fns";
+
+export async function POST(req:Request) {
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature") as string;
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    } catch(error: any){
+        return new NextResponse(`Webhook Error: ${error.message}`, {status: 400});
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+    const address = session?.customer_details?.address;
+
+    const addressComponents = [
+        address?.line1,
+        address?.line2,
+        address?.city,
+        address?.state,
+        address?.postal_code,
+        address?.country,
+    ];
+
+    const addressString = addressComponents.filter((c) => c !== null).join(' ,');
+}
