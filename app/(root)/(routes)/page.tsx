5a024a56101da62f34bbd7ea3ea27ae988204bc5
msrana25 <mnprtrana61@gmail.com>
2023-09-08 21:12:39 -0400

    Completed the admin dashboard

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useEffect } from "react";
 
@@ -8,13 +8,13 @@ const SetupPage = () => {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
 
-
   useEffect(() => {
-    if (!isOpen){
+    if (!isOpen) {
       onOpen();
     }
   }, [isOpen, onOpen]);
-  return null;
-}
 
+  return null;
+};
+ 
 export default SetupPage;
